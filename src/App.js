2329f62ac01e4f0a9aaa180f0812d0b59beb0dde
Scotty Brown <scottybrown98@gmail.com>
2024-04-11 18:20:04 -0500

    Add State in App to hold search result value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 import MainContent from './components/MainContent';
 import Search from './components/Search';
 import Sidebar from './components/Sidebar';
 
 function App() {
+const [searchResults, setSearchResults] = useState("");
+
   return (
     <div className="flex">
       <Sidebar />
-      <MainContent />
-      <Search />
+      <MainContent searchResults={searchResults} />
+      <Search setSearchResults={setSearchResults} />
     </div>
   );
 }
